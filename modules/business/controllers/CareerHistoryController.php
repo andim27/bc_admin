f122f3e71686ddf9137ddcf1642d65dd87bca15f
Aleksandr Gur <aleksandrgur@gmail.com>
2017-05-16 12:39:19 +0300

    https://trello.com/c/pouE5Zxk/476--

---
+++ modules/business/controllers/CareerHistoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\modules\business\controllers;
+
+use app\controllers\BaseController;
+use Yii;
+use app\models\api;
+use app\models\api\user\CareerHistory;
+
+class CareerHistoryController extends BaseController
+{
+    public function actionIndex()
+    {
+        $request = Yii::$app->request->post();
+
+        $dateFrom = isset($request['from']) ? strtotime($request['from']) : time();
+        $dateTo = isset($request['to']) ? strtotime($request['to']) : time();
+
+        $careeHistory = CareerHistory::get(gmdate('d.m.Y', $dateFrom), gmdate('d.m.Y', $dateTo));
+
+        return $this->render('index', [
+            'careerHistory' => $careeHistory,
+            'from' => $dateFrom,
+            'to' => $dateTo
+        ]);
+    }
+}
