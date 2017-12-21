58395799a49e55c93ae29516a310e3773ce9725d
bulich <bulich@do4you.ru>
2017-12-21 05:32:08 +0200

    test worker

---
+++ post-worker.sh
@@ -0,0 +1,4 @@
+rm supervisord.conf 
+rm laravel_worker.conf 
+rm php.ini 
+rm Dockerfile
---
+++ pre-worker.sh
@@ -0,0 +1,4 @@
+cp ./supervisor/supervisord.conf .
+cp ./supervisor/conf.d/laravel_worker.conf .
+cp ./settings/php/php.ini .
+cp ./dockerfiles/worker/Dockerfile .
