b4833486be7590b3f1b087178ff70e44edb531c9
bulich <bulich@do4you.ru>
2018-01-04 20:01:01 +0200

    fix

--- dockerfiles/admin/build.sh
+++ dockerfiles/admin/build.sh
@@ -4,5 +4,5 @@ docker build -t bpt-admin . --no-cache
 docker tag bpt-bc:latest 893041221537.dkr.ecr.eu-central-1.amazonaws.com/bpt-admin:latest
 docker push 893041221537.dkr.ecr.eu-central-1.amazonaws.com/bpt-admin:latest
 rm -r code
-ecs-cli compose -f composer-bc.yml service up
+ecs-cli compose -f composer-admin.yml service up
 #ecs-cli compose -f composer-bc.yml service scale 2
