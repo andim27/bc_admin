1a6288f87a0d5b6686a2c52a15bb449dd7729e30
bulich <bulich@do4you.ru>
2018-01-04 15:23:20 +0200

    fix

--- dockerfiles/admin/build.sh
+++ dockerfiles/admin/build.sh
@@ -1,6 +1,6 @@
-cp -r ..//../app/bc/ ./code/
+cp -r ..//../app/admin/ ./code/
 
-docker build -t bpt-bc . --no-cache
+docker build -t bpt-admin . --no-cache
 #docker tag bpt-bc:latest 893041221537.dkr.ecr.eu-central-1.amazonaws.com/bpt-bc:latest
 #docker push 893041221537.dkr.ecr.eu-central-1.amazonaws.com/bpt-bc:latest
 rm -r code
