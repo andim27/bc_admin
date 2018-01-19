226a18ab12775db37197cd3cc9b09ac28c44f204
bulich <bulich@do4you.ru>
2018-01-20 01:36:24 +0200

    added merge button

---
+++ merge-from-dev.sh
@@ -0,0 +1,10 @@
+mv dockerfiles/ /tmp/
+mv config/params.php /tmp
+mv config/console.php /tmp
+git merge admin-dev
+rm -r dockerfiles
+rm config/params.php
+rm config/console.php
+mv /tmp/dockerfiles/ .
+mv /tmp/params.php ./config/
+mv /tmp/console.php ./config/
