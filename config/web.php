e469c779ed49d029a05679ad2c47b36ddf4d0f0f
bulich <bulich@do4you.ru>
2018-01-20 16:56:55 +0200

    config fix

--- config/web.php
+++ config/web.php
@@ -82,7 +82,7 @@ $config = [
         // for mongodb
         'mongodb' => [
             'class' => '\yii\mongodb\Connection',
-            'dsn' => 'mongodb://gnc:b41uHcdR@bpt-shard-00-00-ezdfa.mongodb.net:27017/gnc?ssl=true&replicaSet=BPT-shard-0&authSource=admin',
+            'dsn' => 'mongodb://gnc:b41uHcdR@bpt-shard-00-00-ezdfa.mongodb.net:27017/gnc?ssl=true&replicaSet=BPT-shard-0&authSource=admin&sslWeakCertificateValidation=true',
 //            'dsn' => 'mongodb://mongo/gnc',
         ],
 
