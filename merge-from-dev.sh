5ea06f672e7382ae13e80970a63ca87be752941f
bulich <bulich@do4you.ru>
2018-02-16 20:06:16 +0200

    temp commit

--- merge-from-dev.sh
+++ merge-from-dev.sh
@@ -1,13 +1,17 @@
-git checkout admin-dev
-git pull origin admin-dev
-git checkout admin
+#git checkout admin-dev
+#git pull origin admin-dev
+#git checkout admin
 mv dockerfiles/ /tmp/
 mv config/params.php /tmp
 mv config/console.php /tmp
-git merge admin-dev
-rm -r dockerfiles
-rm config/params.php
-rm config/console.php
-mv /tmp/dockerfiles/ .
-mv /tmp/params.php ./config/
-mv /tmp/console.php ./config/
+mv config/web.php /tmp
+#git merge admin-dev
+#rm -r dockerfiles
+#rm config/params.php
+#rm config/console.php
+#rm config/web.php
+
+#mv /tmp/dockerfiles/ .
+#mv /tmp/params.php ./config/
+#mv /tmp/console.php ./config/
+#mv /tmp/web.php ./config/
