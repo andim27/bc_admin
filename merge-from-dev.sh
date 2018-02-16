df2a09dbce964bd9eccfac7fd1471079399a1cc9
bulich <bulich@do4you.ru>
2018-02-16 20:12:50 +0200

    merge.sh fix

--- merge-from-dev.sh
+++ merge-from-dev.sh
@@ -1,12 +1,12 @@
-#git checkout admin-dev
-#git pull origin admin-dev
-#git checkout admin
-#mv dockerfiles/ /tmp/
-#mv config/params.php /tmp
-#mv config/console.php /tmp
-#mv config/web.php /tmp
-#git merge admin-dev
-#rm -r dockerfiles
+git checkout admin-dev
+git pull origin admin-dev
+git checkout admin
+mv dockerfiles/ /tmp/
+mv config/params.php /tmp
+mv config/console.php /tmp
+mv config/web.php /tmp
+git merge admin-dev
+rm -r dockerfiles
 rm config/params.php
 rm config/console.php
 rm config/web.php
