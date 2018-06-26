5e7a2b4e35b0fa06fc65b732b9576609390b42b6
Andrey MK <a2772.am@gmail.com>
2018-06-26 12:23:16 +0300

    bug fix __v prop in models/api/Converence,Document

--- models/api/Conference.php
+++ models/api/Conference.php
@@ -69,7 +69,7 @@ class Conference
                 $promotion->id         = $object->_id;
                 $promotion->author     = $object->author;
                 $promotion->lang       = $object->lang;
-                $promotion->v          = $object->__v;
+                $promotion->v          = isset($object->__v)?$object->__v:0;
                 $promotion->isDelete   = $object->isDelete;
                 $promotion->dateOfPublication = strtotime($object->dateOfPublication);
                 $promotion->dateUpdate = strtotime($object->dateUpdate);
