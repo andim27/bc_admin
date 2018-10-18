419a877b95cf7cb51b4f066ee43d5c8cc67c2efd
Aleksandr Gur <aleksandrgur@gmail.com>
2018-10-18 17:39:56 +0300

    Shop orders

---
+++ models/Order.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+use yii\mongodb\ActiveRecord;
+
+/**
+ * Class Order
+ * @package app\models
+ */
+class Order extends ActiveRecord
+{
+    /**
+     * @return string
+     */
+    public static function collectionName()
+    {
+        return 'orders';
+    }
+
+    /**
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            '_id',
+            'orderId',
+            'status',
+            'paymentStatus',
+            'paymentType',
+            'total',
+            'amount',
+            'products',
+            'created_at',
+        ];
+    }
+
+}
