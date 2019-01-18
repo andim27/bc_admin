d050d3a6e805278f42d01c645632fd21b560183f
Grigoriy <gevglevskiy@gmail.com>
2019-01-18 11:26:11 +0000

    requests open html 0.2

--- modules/business/views/showrooms/requests-open.php
+++ modules/business/views/showrooms/requests-open.php
@@ -45,6 +45,42 @@
                 </tr>
             </thead>
             <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td><a class="editRequest" href="#"><i class="fa fa-pencil"></i></a></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td><a class="editRequest" href="#"><i class="fa fa-pencil"></i></a></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td><a class="editRequest" href="#"><i class="fa fa-pencil"></i></a></td>
+                </tr>
 
             </tbody>
         </table>
@@ -54,28 +90,21 @@
 <div class="panel panel-default requestInfo">
   <div class="panel-body">
       <div class="row">
-          <div class="col-sm-12">
-            <p class="m-t m-b">Дата заявки: <strong>26th Mar 2013</strong></p>
-            <p class="m-t m-b">Логин: <strong>mai</strong></p>
-            <p class="m-t m-b">ФИО: <strong>Петрова Светлана</strong></p>
-            <p class="m-t m-b">Страна: <strong>Россия</strong></p>
-            <p class="m-t m-b">Город: <strong>Москва</strong></p>
-            <p class="m-t m-b">Контакты профиля: <strong>Россия петрозаводск +784444144</strong></p>
-            <p class="m-t m-b">Текст с формы: <strong>блабла бла бал ба лаб алаб ал ла абал ба ла ла бюал аб ла бал аб ал бал аб ла</strong></p>
-            <p class="m-t m-b">Статус карьеры (регионан.мен): <strong>Специалист</strong></p>
-            <p class="m-t m-b">Условия 10000/3: <strong>НЕТ</strong></p>
+          <div class="col-sm-12 requestMeta">
+            <p class="m-t m-b">Дата заявки: <strong class="request-date"></strong></p>
+            <p class="m-t m-b">Логин: <strong class="request-login"></strong></p>
+            <p class="m-t m-b">ФИО: <strong class="request-FIO"></strong></p>
+            <p class="m-t m-b">Страна: <strong class="request-country"></strong></p>
+            <p class="m-t m-b">Город: <strong class="request-city"></strong></p>
+            <p class="m-t m-b">Контакты профиля: <strong class="request-contacts"></strong></p>
+            <p class="m-t m-b">Текст с формы: <strong class="request-text"></strong></p>
+            <p class="m-t m-b">Статус карьеры (регионан.мен): <strong class="request-stateCarrer"></strong></p>
+            <p class="m-t m-b">Условия 10000/3: <strong class="request-conditions"></strong></p>
           </div>
         </div>
 
         <div class="row">
             <div class="col-sm-12 imgRequestsContainer">
-                <a href="/images/backgrounds/1.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/1.jpg" class="imgRequests"></a>
-                <a href="/images/backgrounds/2.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/2.jpg" class="imgRequests"></a>
-                <a href="/images/backgrounds/3.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/3.jpg" class="imgRequests"></a>
-                <a href="/images/backgrounds/4.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/4.jpg" class="imgRequests"></a>
-                <a href="/images/backgrounds/5.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/5.jpg" class="imgRequests"></a>
-                <a href="/images/backgrounds/6.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/6.jpg" class="imgRequests"></a>
-                <a href="/images/backgrounds/7.jpg" data-lightbox="roadtrip"><img src="/images/backgrounds/7.jpg" class="imgRequests"></a>
             </div>
         </div>
         <div class="row m-t-lg">
@@ -85,14 +114,14 @@
             </div>
             <div class="col-sm-4">
               <div class="col-sm-12 text-center m-b">
-                <button class="btn btn-default center"><i class="fa fa-cloud-upload text"></i> Добавить файл</button>
+                    <a href="#" data-toggle="ajaxModal">
+                        <i class="fa fa-cloud-upload text" title="Добавить файл"></i> Добавить файл
+                    </a>
               </div>
               <table class="table datagrid m-b-sm requestsFiles">
                   <thead>
                   <tr>
-                      <th>Название файла</th>
-                      <th class="w-69"></th>
-                      <th class="w-69"></th>
+                      <th colspan="3">Прикреплённые файлы</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -117,16 +146,16 @@
             </div>
             <div class="col-sm-3">
                 <label class="control-label"><strong>Статус заявки</strong></label>
-                <select name="account" class="form-control m-b"> 
-                    <option>На рассмотрении</option> 
-                    <option>Отклонено</option> 
-                    <option>Одобрено</option>
+                <select name="account" class="form-control m-b requestStateSelect"> 
+                    <option value="0">На рассмотрении</option> 
+                    <option value="1">Отклонено</option> 
+                    <option value="2">Одобрено</option>
                 </select>
                 <label class="control-label"><strong>Проверяющий</strong></label>
-                <select name="account" class="form-control m-b"> 
-                    <option>Вильховая </option> 
-                    <option>Черногубов</option> 
-                    <option>и т.д.</option>
+                <select name="account" class="form-control m-b requestVerifierSelect"> 
+                    <option value="firely">Вильховая </option> 
+                    <option value="main" >Черногубов</option> 
+                    <option value="1">и т.д.</option>
                 </select>
 
                 <!-- ?= Select2::widget([
@@ -139,6 +168,8 @@
                     ]
                     ]);
                 ?>-->
+
+                
             </div>
         </div>
         <div class="row">
@@ -149,52 +180,134 @@
   </div>
 </div>
 
+<div class="modal-dialog" style="display:none;">
+    <div class="modal-content">
+        <div class="modal-body">
+            <button type="button" class="close" data-dismiss="modal">x</button>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label>Название файла</label>
+                        <input type="text" class="form-control" name="product" value="" required="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="form-group">
+                        <input type="file" class="filestyle" data-buttonText="Выбрать файл"  data-iconName="fa fa-cloud-upload"
+                                data-classButton="btn btn-default m-b-5" data-classInput="form-control inline input-s">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
 <?php $this->registerCssFile('/css/lightbox.css', ['position' => yii\web\View::POS_HEAD]); ?>
 <?php $this->registerJsFile('/js/lightbox/lightbox.js', ['position' => yii\web\View::POS_END]); ?>
+<?php $this->registerJsFile('/js/bootstrap-filestyle.min.js', ['position' => yii\web\View::POS_END]); ?>
 
 <script>
-    
-    var table = $('.table-users');
 
-    table = table.dataTable({
-        language: TRANSLATION,
-        lengthMenu: [ 25, 50, 75, 100 ],
-        "processing": true,
-        "serverSide": true,
-        "ajax": {
-            "url": '/' + LANG + '/business/user'
+    var dataReq = {
+        metadata : {
+            date : '26.12.1996',
+            login : 'mai',
+            FIO : 'Петрова Светлана',
+            country : 'Россия',
+            city : 'Москва',
+            contacts : 'Россия петрозаводск +784444144',
+            text : 'блабла бла бал ба лаб алаб ал ла абал ба ла ла бюал аб ла бал аб ал бал аб ла',
+            stateCarrer: 'Специалист 2',
+            conditions : 'НЕТ'
         },
-        "columns": [
-            {"data": "created"},
-            {"data": "username"},
-            {"data": "full_name"},
-            {"data": "country_city"},
-            {"data": "phoneNumber"},
-            {"data": "phoneNumber"},
-            {"data": "rank"},
-            {"data": "phoneNumber"},
-            {"data": "phoneNumber"},
+        state: 1,
+        verifierID: 'main',
+        files : [
             {
-              "data": null,
-              "defaultContent": '<a class="editRequest" href="#"><i class="fa fa-pencil"></i></a>'
+                id : 'dsjfhjshfjhsdhf22',
+                name : 'Договор лизинга',
+                src : '/requestsFiles/mai/Договор1.pdf'
+            },
+            {
+                id : 'dsjfhjshfjhsdhf21111',
+                name : 'Договор лизинга 2',
+                src : '/requestsFiles/mai/Договор2.pdf'
             }
         ],
-        "order": [[ 0, "asc" ]]
-    })
-
+        images : [
+            '/images/backgrounds/1.jpg',
+            '/images/backgrounds/2.jpg',
+            '/images/backgrounds/3.jpg',
+            '/images/backgrounds/4.jpg',
+            '/images/backgrounds/5.jpg',
+            '/images/backgrounds/6.jpg',
+            '/images/backgrounds/7.jpg'
+        ]
+    };
+    
     $('table').on('click','.editRequest',function(){
         console.log($(this).parents('tr')[0].rowIndex);
         // ну тут подгружаем данные по заявке
+        
 
-       // и закидываем инфу в форму requestInfo
+        // тут закинули инфо в мета
 
+        for (const key in dataReq.metadata) {
+            if (dataReq.metadata.hasOwnProperty(key)) {
+                const element = dataReq.metadata[key];
+                $(`.request-${ key }`).html(element);
+            }
+        }
+        
+        // тут собрали картиночный див
+        
+        $('.imgRequestsContainer').empty();
+
+        if (dataReq.images.length > 0 ) {
+            for (const image of dataReq.images) {
+                $('.imgRequestsContainer').append(`<a href="${ image }" data-lightbox="roadtrip"><img src="${ image }" class="imgRequests"></a>`);
+            }
+        }
+
+        // тут собрали файлы
+        $('.requestsFiles>tbody').empty();
+        if (dataReq.files.length > 0 ) { 
+            for (const file of dataReq.files) {
+                console.log(file.name);
+                $('.requestsFiles>tbody').append(`
+                    <tr>
+                        <td>${ file.name }</td>
+                        <td class="w-69"><a href="${ file.src }" class="btn btn-success btn-sm">Скачать</a></td>
+                        <td class="w-69"><a href="#" data-file="${ file.id }" class="btn btn-danger btn-sm deleteFile">Удалить</a></td>
+                    </tr>
+                `);
+            }            
+        }
+
+        // статус заявки
+        $('.requestStateSelect').val(dataReq.state);
+
+        // проверяющий
+        $('.requestVerifierSelect').val(dataReq.verifierID);
 
        // и отображаем её
         $('.requestInfo').show();
+
     } );
 
     $('.requestInfo').on('click','.saveRequest',function(){
         console.log('saveRequest');
+        // тут пушим новые данные в заявку
+
+
+
+        $('.requestInfo').hide();
     } );
 
 
