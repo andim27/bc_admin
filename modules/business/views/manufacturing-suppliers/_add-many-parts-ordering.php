b9ee8afd8154716c629f045fe35197d0ff8a777b
andrey <a2772.am@gmail.com>
2019-03-12 17:40:47 +0200

    pick up parts

--- modules/business/views/manufacturing-suppliers/_add-many-parts-ordering.php
+++ modules/business/views/manufacturing-suppliers/_add-many-parts-ordering.php
@@ -63,8 +63,10 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
                             'class'=>'form-control',
                             'id'=>'selectChangeStatus',
                             'required'=>'required',
+                            'onchange' =>'clearError();',
                             'options' => [
-                                '' => ['disabled' => true]
+                                ''         => ['disabled' => true],
+
                             ]
                         ])?>
 
@@ -80,6 +82,7 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
                             'options' => [
                                     'id'=>'cur_good',
                                 'placeholder' => 'Выберите товар',
+                                'onchange'=>'clearError();'
 
                             ]
                         ]);
@@ -105,6 +108,7 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
                             'class'=>'form-control',
                             'min'=>'1',
                             'step'=>'1',
+                            'onchange'=>'calcNumberPrice();'
                         ])?>
                     </div>
                     <div class="col-md-2">
@@ -127,6 +131,8 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
                             'class'=>'form-control',
                             'min'=>'0.01',
                             'step'=>'0.01',
+                            'onblur'  =>'clearError();',
+                            'onchange'=>'calcNumberPrice();'
                         ])?>
                     </div>
                      <div class="col-md-1">
@@ -136,6 +142,20 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
                 </div>
             </div>
             <br>
+            <div class="row b-bottom-dotted" id="part-totals" style="display: none">
+                <div class="col-md-5">
+                    <h4 class="text-primary text-right">Итого:</h4>
+                </div>
+                <div class="col-md-2 ">
+                    <h4 id="number-total-sum" class="text-primary text-left">00</h4>
+                </div>
+                <div class="col-md-2 offset-right-1">
+                    <h4 class="text-primary text-right">Стоимость:</h4>
+                </div>
+                <div class="col-md-2 ">
+                    <h4 id="price-total-sum" class="m-bottom-20">0.00</h4>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-md-4 offset-left-4">
                     Дата прибития
@@ -165,7 +185,7 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
     $(".datepicker-input").datepicker();
 
     arrayGoodsComposite = <?=json_encode($listGoodsWithComposite)?>;
-    $('#part-accessors').multiselect();
+    //$('#part-accessors').multiselect();
     $("#partsAccessoriesId").on("change",function () {
 
         clearError();
@@ -193,14 +213,13 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
     p_items_arr=[];
     function addPart() {
         if ( $('#cur_good').select2('data')[0].id ==''){
-            alert('Выберите товар');
+            alertError('Выберите товар');
             return;
         }
         $('#part-items').show();
+        $('#part-totals').show();
         var cnt =  $('#part-items').children().length;
         p_items_cnt = cnt;
-        console.log('cnt='+cnt);
-
         if (cnt >= 1) {
             $('#part-item-0').clone().appendTo('#part-items');
 
@@ -210,31 +229,97 @@ $listGoodsWithComposite = PartsAccessories::getListPartsAccessoriesWithComposite
             var cur_goods_value = $('#cur_good').select2('data')[0].id;
             $('#part-item-'+cnt+' h4').html(cur_goods_text);
             $('#part-item-'+cnt).attr('data-part_id',cur_goods_value);
-            console.log('p_items_cnt='+p_items_cnt);
             p_items_cnt =p_items_cnt+1;
         }
-        //var p_item = $('#part-item-0').clone().prop("part-item-0", "part-item-1");
-        //$('#part-items').append(p_item);
-
-        //$('#part-item-0').clone().prop("part-item-0", "part-item-"+cnt).appendTo('#part-items');
-
+        calcNumberPrice();
     }
     function delPart(el) {
         var cnt =  $('#part-items').children().length;
         if (cnt > 0) {
             $(el).parent().parent().remove();
+        } else {
+            $('#part-totals').hide();
         }
+        calcNumberPrice();
 
-
+    }
+    function calcNumberPrice() {
+        var price_all=0;
+        var number_all=0;
+        var p_items =$('#part-items');
+        var cnt =  p_items.children().length;
+        for (var i=1;i<cnt;i++) {
+            var price_item    = parseFloat(p_items.children().eq(i).find('input[name="price"]').val());//parseFloat($('#part-item-'+i+' input[name="price"]').val());
+            var number_item   = parseFloat(p_items.children().eq(i).find('input[name="number"]').val());
+            price_all+=price_item;
+            number_all+=number_item;
+        }
+        $('#number-total-sum').html(''+number_all.toFixed(2));
+        $('#price-total-sum').html(''+price_all.toFixed(2));
+    }
+    function showError(name,i) {
+        var name_rus='';
+        if (name =='part_number') {
+            name_rus='Колличество';
+            $('#part-item-'+i+' input[name="number"]').focus();
+        }
+        if (name =='part_price') {
+            name_rus='Стоимость';
+            $('#part-item-'+i+' input[name="price"]').focus();
+        }
+        if (name =='suppliers') {
+            name_rus = 'Поставщик';
+            $('select[name="suppliers_performers_id"]').focus();
+        }
+        if (name =='parts_accessories') {
+            name_rus = 'Товар';
+            $('select[name="parts_accessories_id"]').focus();
+        }
+        name_rus = '<strong>'+name_rus+'</strong>';
+        alertError('Некорректное значение поля '+name_rus);
+    }
+    function checkPartItems() {
+        var res=true;
+        if ($('select[name="suppliers_performers_id"]').val() == null) {
+            showError('suppliers',0);
+            return false;
+        }
+        if ($('select[name="parts_accessories_id"]').val() == '') {
+            showError('parts_accessories',0);
+            return false;
+        }
+        var cnt =  $('#part-items').children().length;
+        for (var i=1;i<=cnt;i++) {
+            part_number   = $('#part-item-'+i+' input[name="number"]').val();
+            if (part_number <=0) {
+                showError('part_number',i);
+                return false;
+            }
+            part_price    = $('#part-item-'+i+' input[name="price"]').val();
+            if (part_price <=0) {
+                showError('part_price',i);
+                return false;
+            }
+        }
+        return res;
     }
     function pickItems() {
         var part_items=[
-            {'part_id':123,'part_number':1,'part_currency':'eur','part_price':100},
-            {'part_id':456,'part_number':2,'part_currency':'eur','part_price':200},
-        1];
+            //{'part_id':123,'part_number':1,'part_currency':'eur','part_price':100},
+            //{'part_id':456,'part_number':2,'part_currency':'eur','part_price':200},
+        ];
+        var cnt =  $('#part-items').children().length;
+        for (var i=0;i<=cnt;i++) {
+            part_id       = $('#part-item-'+i).attr('data-part_id');
+            part_number   = $('#part-item-'+i+' input[name="number"]').val();
+            part_currency = $('#part-item-'+i+' select[name="currency"]').val();
+            part_price    = $('#part-item-'+i+' input[name="price"]').val();
+            part_items.push({'part_id':part_id,'part_number':part_number,'part_currency':part_currency,'part_price':part_price});
+        }
         return part_items;
     }
     function saveParts() {
+        if (checkPartItems()==false) return;
         $('#s_btn').attr('disabled',true);
         var part_items = pickItems();
         var url="/<?=Yii::$app->language?>/business/manufacturing-suppliers/save-many-parts-ordering";
